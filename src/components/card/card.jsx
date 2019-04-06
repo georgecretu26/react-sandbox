b270706d6f47245c3800256611fa2e309666db29
George Cretu <narciscretu26@gmail.com>
2019-04-06 19:16:26 +0200

    remove dummy data and use data from the backend

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -5,12 +5,18 @@ import './style.less';
 const Card = props => {
     console.log('Card is initiated', props);
     
-    const {title, imgSrc, dateOfPublish, description} = props.props;
+    const {
+        title, 
+        img_src: imgSrc, 
+        created_date: dateOfCreation, 
+        published_date: dateOfPublish, 
+        description
+    } = props.props;
     return(
         <div className="container">
             <div className="header">
                 <p className="title">{title}</p>
-                <p className="date">{dateOfPublish}</p>
+                <p className="date">{dateOfPublish ? dateOfPublish : dateOfCreation}</p>
             </div>
             <img src={imgSrc} alt={title}></img>
             <p className="description">{description}</p>
--- src/containers/projects/index.jsx
+++ src/containers/projects/index.jsx
@@ -3,44 +3,47 @@ import React, { Component } from 'react';
 import Card from './../../components/card/card';
 import axios from 'axios';
 
-const projects = [
-    {
-        id: 1, 
-        title: 'Website Project', 
-        imgSrc:'https://dummyimage.com/mediumrectagle/d60bd6/ffffff&text=Website+react+JS', 
-        dateOfPublish: '26th of March, 2018', 
-        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere animi ullam minima natus sequi porro ab eum nostrum necessitatibus nihil, numquam praesentium nobis dolore, expedita pariatur, dolorum sed error at."
-    },
-    {
-        id: 2, 
-        title: 'Mobile Project', 
-        imgSrc:'https://dummyimage.com/mediumrectagle/d60bd6/ffffff&text=Mobile+react+JS', 
-        dateOfPublish: '12th of April, 2018', 
-        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere animi ullam minima natus sequi porro ab eum nostrum necessitatibus nihil, numquam praesentium nobis dolore, expedita pariatur, dolorum sed error at."
-    },
-]
+// const projects = [
+//     {
+//         id: 1, 
+//         title: 'Website Project', 
+//         imgSrc:'https://dummyimage.com/mediumrectagle/d60bd6/ffffff&text=Website+react+JS', 
+//         dateOfPublish: '26th of March, 2018', 
+//         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere animi ullam minima natus sequi porro ab eum nostrum necessitatibus nihil, numquam praesentium nobis dolore, expedita pariatur, dolorum sed error at."
+//     },
+//     {
+//         id: 2, 
+//         title: 'Mobile Project', 
+//         imgSrc:'https://dummyimage.com/mediumrectagle/d60bd6/ffffff&text=Mobile+react+JS', 
+//         dateOfPublish: '12th of April, 2018', 
+//         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere animi ullam minima natus sequi porro ab eum nostrum necessitatibus nihil, numquam praesentium nobis dolore, expedita pariatur, dolorum sed error at."
+//     },
+// ]
 
 class Projects extends Component{
     constructor(){
         super();
         this.state = {
-            projects: projects,
+            projects: [],
         };
     }
 
     componentWillMount(){
-        axios.get('http://localhost:8000/project/')
-        .then(result => console.log(result))
+        axios.get('http://localhost:8000/projects/')
+        .then(result => {
+            const { data: projects } = result;
+            this.setState({ projects });
+        })
         .catch(err => console.log(err))
     }
 
-    renderProjects(obj){
-        if(obj.length > 0){
-            obj.map(project => (
-                <Card key={project.id} props={project} />
-            ))
-        }
-    }
+    // renderProjects(obj){
+    //     if(obj.length > 0){
+    //         obj.map(project => (
+    //             <Card key={project.id} props={project} />
+    //         ))
+    //     }
+    // }
     render(){
         console.log('state', this.state);
         
