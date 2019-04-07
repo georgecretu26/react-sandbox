f4c8ce41bc9c53ac0e4c1e0ac630e5d1fb4a7ca9
George Cretu <narciscretu26@gmail.com>
2019-04-07 21:07:54 +0200

    add class name to the main content

--- src/containers/about/index.jsx
+++ src/containers/about/index.jsx
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 class About extends Component{
     render(){
         return(
-            <div>
+            <div className="content">
                 About page
             </div>
         )
